893cbc18a1d583ffb60a01d4fbdc11223ff949f1
Chisom Emmanuel <mailemmanuel00@gmail.com>
2023-10-05 14:09:20 +0100

    Footer credits

--- src/containers/common/Footer.jsx
+++ src/containers/common/Footer.jsx
@@ -1,13 +1,17 @@
 import { Typography } from "@/components";
+import Link from "next/link";
 
 const Footer = () => {
-    return (
-      <>
-        <section className="flex justify-center my-10">
-          <Typography variant="p.medium">Copyright © 2023 StartEase.</Typography>
-        </section>
-      </>
-    );
-}
+  return (
+    <>
+      <section className="flex justify-center my-10">
+        <Typography variant="p.medium">Copyright © 2023 StartEase.</Typography>
+        <Typography variant="p.medium">
+          &nbsp; Page by <Link href="https://twitter.com/8mmnuel">eight.</Link>{" "}
+        </Typography>
+      </section>
+    </>
+  );
+};
 
 export default Footer;
